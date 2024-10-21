438430e711cd9135fbd610be159953ad03600114
Aoxcis <gregoirepaul@laposte.net>
2024-10-21 16:33:33 +0200

    config

--- import_aliments/config.php
+++ import_aliments/config.php
@@ -2,8 +2,8 @@
 
 
     define('_MYSQL_HOST','127.0.0.1');
-    define('_MYSQL_PORT',3306);
+    define('_MYSQL_PORT',8889);
     define('_MYSQL_USER','root');
     define('_MYSQL_DBNAME','projet_idaw');
-    define('_MYSQL_PASSWORD','');
+    define('_MYSQL_PASSWORD','root');
 
