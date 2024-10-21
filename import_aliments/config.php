6b495b5f64f6d939df86f79ac01ef4440d69e25b
Aoxcis <gregoirepaul@laposte.net>
2024-10-21 15:37:05 +0200

    import utilisateurs a peu près fonctionnel

--- import_aliments/config.php
+++ import_aliments/config.php
@@ -4,6 +4,6 @@
     define('_MYSQL_HOST','127.0.0.1');
     define('_MYSQL_PORT',8889);
     define('_MYSQL_USER','root');
-    define('_MYSQL_DBNAME','dbtest');
+    define('_MYSQL_DBNAME','projet_idaw');
     define('_MYSQL_PASSWORD','root');
 
