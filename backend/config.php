89efd60086bf31d1a1854f7c2731b7fe03540200
Aoxcis <gregoirepaul@laposte.net>
2024-10-22 15:39:12 +0200

    fix gitignore

--- backend/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('_MYSQL_HOST', 'localhost-projet');
-define('_MYSQL_PORT', '3306');
-define('_MYSQL_USER', 'root');
-define('_MYSQL_PASSWORD', '');
-define('_MYSQL_DBNAME', 'projet_idaw');
