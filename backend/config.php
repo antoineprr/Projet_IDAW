bf3b8a3bb280fba4499d9aec1651c21ba1d7d2cb
Aoxcis <gregoirepaul@laposte.net>
2024-10-22 15:35:03 +0200

    changement structure pour suivre consignes

---
+++ backend/config.php
@@ -0,0 +1,6 @@
+<?php
+define('_MYSQL_HOST', 'localhost-projet');
+define('_MYSQL_PORT', '3306');
+define('_MYSQL_USER', 'root');
+define('_MYSQL_PASSWORD', '');
+define('_MYSQL_DBNAME', 'projet_idaw');
