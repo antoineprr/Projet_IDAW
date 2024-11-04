6031dfd75409b3bc49ed1e5c3435b6e76db395c9
Aoxcis <gregoirepaul@laposte.net>
2024-11-04 09:04:04 +0100

    index.php

---
+++ frontend/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>i mangez mieux</title>
+    <link rel="stylesheet" type="text/css" href="css/boostrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg bg-primary">
+            <a class="navbar-brand" href="#">i mangez mieux</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="index.php">Accueil</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="journal.php">Aliments</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="profil.php">Type Aliments</a>
+                    </li>
+                </ul>
+            </div>
