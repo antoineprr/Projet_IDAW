4c0145a18e74d216c0936331594798679c818c70
Aoxcis <gregoirepaul@laposte.net>
2024-11-04 09:29:37 +0100

    feat: template footer et header

---
+++ frontend/footer_template.php
@@ -0,0 +1,26 @@
+<footer class="bg-dark text-white mt-5 p-4 text-center">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <h5>A propos</h5>
+                <p>Projet IDAW</p>
+            </div>
+            <div class="col-md-4">
+                <h5>Contact</h5>
+                <p>Email: info@example.com</p>
+                <p>Phone: +123 456 7890</p>
+            </div>
+            <div class="col-md-4">
+                <h5>Follow Us</h5>
+                <a href="#" class="text-white me-2"><i class="fab fa-facebook-f">Facebook</i></a>
+                <a href="#" class="text-white me-2"><i class="fab fa-twitter">Twitter</i></a>
+                <a href="#" class="text-white"><i class="fab fa-instagram">Instagram</i></a>
+            </div>
+        </div>
+        <div class="row mt-3">
+            <div class="col-md-12">
+                <p>&copy; <?php echo date("Y"); ?> IMT Nord Europe. All rights reserved.</p>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ frontend/header_template.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>i mangez mieux</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarColor01">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Accueil
+            <span class="visually-hidden">(current)</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="profil.php">Profil</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="journal.php">Journal</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="aliments.php">Aliments</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="connexion.php">Deconnexion</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-sm-2" type="search" placeholder="Search">
+        <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
