3bb195598c8cd50dd7ebbb3245eceb7699feb6b0
Antoine Poirier <antoine.poirier@etu.imt-nord-europe.fr>
2024-11-04 08:59:41 +0100

    feat : add style + connexion page

--- frontend/connexion.php
+++ frontend/connexion.php
@@ -4,18 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Connexion</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+
 </head>
 <body>
     <div class="container">
         <h2>Connexion</h2>
         <form action="login.php" method="post">
             <div class="form-group">
-                <label for="username">Nom d'utilisateur:</label>
+                <label for="username">Nom d'utilisateur :</label>
                 <input type="text" id="username" name="username" required>
             </div>
             <div class="form-group">
-                <label for="password">Mot de passe:</label>
+                <label for="password">Mot de passe :</label>
                 <input type="password" id="password" name="password" required>
             </div>
             <button type="submit">Se connecter</button>
