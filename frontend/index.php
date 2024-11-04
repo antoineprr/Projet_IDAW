47783f8797a735e9c82cdffd9428714636a1bc85
Aoxcis <gregoirepaul@laposte.net>
2024-11-04 11:48:00 +0100

    feat: tab calories/ratio index.php

--- frontend/index.php
+++ frontend/index.php
@@ -1,3 +1,48 @@
 <?php
 require_once('header_template.php');
+?>
+
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-md-12 text-center">
+            <h1>Bienvenue sur notre site</h1>
+            <p class="lead">Découvrez nos produits et services exceptionnels.</p>
+        </div>
+    </div>
+    <div class="row mt-4">
+        <div class="col-md-4">
+            <div class="card">
+                <img src="path/to/image1.jpg" class="card-img-top" alt="Image 1">
+                <div class="card-body">
+                    <h5 class="card-title">Produit 1</h5>
+                    <p class="card-text">Description du produit 1.</p>
+                    <a href="#" class="btn btn-primary">En savoir plus</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <img src="path/to/image2.jpg" class="card-img-top" alt="Image 2">
+                <div class="card-body">
+                    <h5 class="card-title">Produit 2</h5>
+                    <p class="card-text">Description du produit 2.</p>
+                    <a href="#" class="btn btn-primary">En savoir plus</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <img src="path/to/image3.jpg" class="card-img-top" alt="Image 3">
+                <div class="card-body">
+                    <h5 class="card-title">Produit 3</h5>
+                    <p class="card-text">Description du produit 3.</p>
+                    <a href="#" class="btn btn-primary">En savoir plus</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
 require_once('footer_template.php');
+?>
